060dac68eb1923b6f33d30634bf63ca6a871e62f
felipesantanafs <145938001+felipesantanafs@users.noreply.github.com>
2025-11-21 21:21:03 -0300

    chore: atualizar baseUrl para org

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "pt-BR",
-    baseUrl: "https://feadev.github.io",
+    baseUrl: "https://felipesantanafs.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
